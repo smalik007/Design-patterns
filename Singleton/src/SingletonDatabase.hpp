a761b476e9d6eff04e2d8a6bd25ec698959fbe84
Suhail <suhail.m@greyorange.com>
2021-06-15 23:26:43 +0530

    Singleton problem, and dependency injection to solve it

--- Singleton/src/SingletonDatabase.hpp
+++ Singleton/src/SingletonDatabase.hpp
@@ -10,10 +10,22 @@
 
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
-class SingletonDatabase {
+
+/* To solve the problem of testing RecordFinder::total_population we will use dependency injection */
+/* For that we define a Base class */
+
+class DataBase {
+ public:
+  virtual int getPopulation(const string& name) = 0;
+};
+
+/* Now we will Inherit this here */
+class SingletonDatabase : public DataBase {
   unordered_map<string, int> map_db_;
   /* Keep the constructor private, and expose an API which will return singleton instance */
   SingletonDatabase() {
@@ -38,5 +50,44 @@ class SingletonDatabase {
     return db;
   }
 
-  int getPopulation(const string& city) { return map_db_[city]; }
+  int getPopulation(const string& city) override { return map_db_[city]; }
+};
+
+struct SingletonRecordFinder {
+  int totalPop(vector<string> caps) {
+    int res{0};
+    for (auto& c : caps) {
+      res += SingletonDatabase::getInstance().getPopulation(c);
+    }
+    return res;
+  }
+};
+
+/* we will also define a DummyData Base */
+class DummyDataBase : public DataBase {
+  unordered_map<string, int> map_db_;
+
+ public:
+  /* This will have a dummy data */
+  DummyDataBase() {
+    map_db_["alpha"] = 1;
+    map_db_["beta"] = 2;
+    map_db_["gamma"] = 3;
+  }
+  int getPopulation(const string& name) override { return map_db_[name]; }
+};
+
+/* This time we will have a Configurable Recordfinder that we can test as a unit test, without having to rely on a production data. We can test the API using the dummy data base and the same time can
+ * also use it for singleton class as well */
+struct ConfigurableRecordFinder {
+  DataBase& db;
+  ConfigurableRecordFinder(DataBase& db) : db(db) {}
+
+  int totalPop(vector<string> caps) {
+    int res{0};
+    for (auto& c : caps) {
+      res += db.getPopulation(c);
+    }
+    return res;
+  }
 };
--- Singleton/src/main.cpp
+++ Singleton/src/main.cpp
@@ -6,6 +6,8 @@
  *
  */
 
+#include <gtest/gtest.h>
+
 #include <iostream>
 
 #include "SingletonDatabase.hpp"
@@ -14,9 +16,42 @@ using namespace std;
 
 void testSingletonDatabase() { cout << "Delhi pop : " << SingletonDatabase::getInstance().getPopulation("Delhi") << endl; }
 
-int main() {
+/* The Disadvantage of using Singleton patter:
+  Here there's we only wanted to test SingletonRecordFinder::totalPop, but due to the strong coupling of singleton class we are bound use the data from the data.txt file, which makes it an
+  intergration test rather than an unit test. Furthermore relying on the production data for a unit test is horrible.   */
+TEST(RecordFinderTest, SingletonTotalPopTest) {
+  SingletonRecordFinder rf;
+  vector<string> names = {"Delhi", "Mumbai"};
+  int res = rf.totalPop(names);
+  int exp = 12345 + 14235;
+  ASSERT_EQ(exp, res) << " Expected : " << exp << "got : " << res;
+}
+
+/* This will be now testing the record finder as a unit test */
+TEST(ConfRecordFinderTest, ConfTotalPopTest) {
+  DummyDataBase db;
+  ConfigurableRecordFinder rf(db);
+  vector<string> names = {"alpha", "beta", "gamma"};
+  int res = rf.totalPop(names);
+  int exp = 1 + 2 + 3;
+  ASSERT_EQ(exp, res) << " Expected : " << exp << "got : " << res;
+}
+
+/* We can also use this ConfigurableRecord finder for our singletom class, But as a test it will be integration test. */
+TEST(ConfRecordFinderTest2, ConfTotalPopTest2) {
+  ConfigurableRecordFinder rf(SingletonDatabase::getInstance());
+  vector<string> names = {"Delhi", "Mumbai"};
+  int res = rf.totalPop(names);
+  int exp = 12345 + 14235;
+  ASSERT_EQ(exp, res) << " Expected : " << exp << "got : " << res;
+}
+
+int main(int agc, char* agv[]) {
   /* Main */
 
+  testing::InitGoogleTest(&agc, agv);
+
   testSingletonDatabase();
-  return 0;
+
+  return RUN_ALL_TESTS();
 }
