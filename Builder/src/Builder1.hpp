409a70c18d6a0366b83e90f9cded795c1de366a5
mosu_195 <mosu@blue-ocean-robotics.com>
2022-08-29 16:59:55 +0200

    core idea for builder

---
+++ Builder/src/Builder1.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class HTMLElemnt
+{
+
+    const size_t indent_ = 2;
+
+public:
+    string name_, text_;
+    vector<HTMLElemnt> elements_;
+
+    HTMLElemnt() {}
+    HTMLElemnt(const string &name, const string &text) : name_(name), text_(text) {}
+
+    string str(int indent = 0) const
+    {
+        ostringstream oss;
+        string i(indent_ * indent, ' ');
+        oss << i << "<" << name_ << ">" << endl;
+
+        if (text_.size() > 0)
+            oss << string(indent_ * (indent + 1), ' ') << text_ << endl;
+
+        // recursive call to the this func
+        for (const auto &e : elements_)
+        {
+            oss << e.str(indent + 1);
+        }
+
+        oss << i << "</" << name_ << ">" << endl;
+        return oss.str();
+    }
+};
+
+class HTMLBuilder
+{
+    HTMLElemnt root;
+
+public:
+    HTMLBuilder(const string &root_name)
+    {
+        root.name_ = root_name;
+    }
+
+    void add_children(string child_name, string child_text)
+    {
+        HTMLElemnt e{child_name, child_text};
+        root.elements_.emplace_back(e);
+    }
+
+    string str() const
+    {
+        return root.str();
+    }
+};
--- Builder/src/main.cpp
+++ Builder/src/main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <sstream>
 
 #include "BuilderHTML.hpp"
 #include "BuilderPizza.hpp"
@@ -8,10 +9,41 @@
 #include "PersonAddressBuilder.hpp"
 #include "PersonBuilder.hpp"
 #include "PersonJobBuilder.hpp"
+#include "Builder1.hpp"
 
 using namespace std;
 
-void testBuilder() {
+void motivation()
+{
+  auto text = "hello";
+  string output;
+  output += "<p>";
+  output += text;
+  output += "</p>";
+  cout << output << endl;
+
+  // more intitutive be using oss string string
+  string words[] = {"hello", "world"};
+  ostringstream oss;
+  oss << "<ul>";
+  for (auto w : words)
+  {
+    oss << " <li>" << w << "</li>";
+  }
+  oss << "</ul>";
+  cout << oss.str() << endl;
+}
+
+void testBuilder1()
+{
+  HTMLBuilder builder{"ul"};
+  builder.add_children("li", "hello");
+  builder.add_children("li", "world");
+  cout << builder.str();
+}
+
+void testBuilder()
+{
   pizza::CookDirector cook;
   pizza::MargheritaConcreteBuilder margheritaPizza;
   pizza::SpicyConcreteBuilder spicyPizza;
@@ -35,7 +67,8 @@ void testBuilder() {
   cout << "Bill Amount : " << cook.billPizza() << endl;
 }
 
-void testBuilderHTML() {
+void testBuilderHTML()
+{
   cout << html::P{html::IMG("https://www.pokemon.com/us/pokedex/pikachu")};
 
   cout << endl;
@@ -43,19 +76,25 @@ void testBuilderHTML() {
                       html::TableRow{html::TableData("John"), html::TableData("25"), html::TableData("M")}, html::TableRow{html::TableData("Aysha"), html::TableData("21"), html::TableData("F")}};
 }
 
-void testPersonBuilder() {
+void testPersonBuilder()
+{
   Person john = Person::create().lives().at("123 Baker Street").with_postcode("124566").in("London").works().at("ABC").as_a("Software Engineer").earning(10e3);
   cout << john << endl;
 }
 
-void testCodeBuilder() {
+void testCodeBuilder()
+{
   auto cb = CodeBuilder{"Rectangle"}.add_field("length", "int").add_field("width", "int");
   cout << cb;
 }
 
-int main() {
-  //   testBuilder();
-  //   testBuilderHTML();
-  testPersonBuilder();
-  testCodeBuilder();
+int main()
+{
+  // motivation();
+  testBuilder1();
+  // testBuilderHTML();
+  // testBuilder();
+
+  // testPersonBuilder();
+  // testCodeBuilder();
 }
